9fd4d2a790e2fef84925357199b98e36e5fa71a8
Veshal Raj <veshalraj1307@gmail.com>
2023-11-26 11:56:05 +0530

    without using immer

---
+++ nested-state.js
@@ -0,0 +1,43 @@
+import redux from 'redux'
+
+const initalState = {
+    name: 'Veshal',
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        state: "CA",
+    },
+}
+
+const STREET_UPDATED = 'STREET_UPDATED'
+
+const updateStreet = (street) => {
+    return {
+        type: STREET_UPDATED,
+        payload: street
+    }
+}
+
+const reducer = (state = initalState, action) => {
+    switch (action.type) {
+        case STREET_UPDATED:
+            return {
+                ...state,
+                address: {
+                    ...state.address,
+                     street: action.payload
+                    },
+                }
+        default: {
+            return state
+        }
+    }
+}
+
+const store = redux.createStore(reducer)
+console.log('Initial State ', store.getState())
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated State ', store.getState())
+})
+store.dispatch(updateStreet('456 Main St'))
+unsubscribe()
